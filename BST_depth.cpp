aa749c31f45d302b6663393d6c8727fec7928589
Mariahsal <89291278+Mariahsal@users.noreply.github.com>
2022-11-15 17:10:49 -0800

    Update BST_depth.cpp

--- BST_depth.cpp
+++ BST_depth.cpp
@@ -6,12 +6,11 @@
  
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        if(!root) return 0;
-        int maxLeft = maxDepth(root->left);
-        int maxRight = maxDepth(root->right);
-        return max(maxLeft, maxRight)+1;
+    int maxDepth(TreeNode* root) { // Function maxDepth() that takes in a node of a binary tree. 
+        if(!root) return 0; //If the node we take in is a null pointer, then there is no depth, therefore return 0.
+        int maxLeft = maxDepth(root->left); //This is the recursive case, assuming that the tree is filled with nodes.
+        int maxRight = maxDepth(root->right); //We check the left and right children. 
+                                             //These lines recursively call the same function we have to go depth first til bottom of the tree is reached.
+        return max(maxLeft, maxRight)+1; //Returns the max height from left and right.
     }
 };
-
-/**
